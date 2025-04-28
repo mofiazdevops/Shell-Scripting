7b24f99c11fc15bdc78e22d5f3e1f4584e1282bb
mofiazdevops <mofiaz4000@gmail.com>
2025-04-28 01:01:41 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-1"
+  
+}
+
+resource "aws_instance" "terraform" {
+    ami = var.ami_value
+    instance_type = var.instance_type_value
+    subnet_id = var.subnet_id_value
+  
+}
