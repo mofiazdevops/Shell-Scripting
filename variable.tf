260a963e95c081500a854b4626740b410703c564
mofiazdevops <mofiaz4000@gmail.com>
2025-04-28 00:59:50 -0700

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,14 @@
+variable "ami_value" {
+    description = "value for the ami"
+}
+
+variable "instance_type_value" {
+    description = "value for the ec2 instance"
+  
+}
+
+variable "subnet_id_value" {
+    description = "value for subnet_id"
+
+  
+}
